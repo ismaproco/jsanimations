6b3b9627aeb843c912bf77ae6d37be884e2e22c0
Ismael <ismaproco@gmail.com>
2016-09-27 23:29:34 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JS animations using easing
+
+This is an example application of the usage of javascript animations using easing functions.
+
---
+++ index.js
@@ -0,0 +1,6 @@
+// init script
+//import Calc from './jsx/Calc.jsx';
+
+var main = document.querySelector('#main');
+
+main.innerText = 'Hello World!!!';
